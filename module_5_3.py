aaa47ad519f64cc89523fbdb99f32ae1ea6f6d45
Aabushkevich <abushkevich9063@gmail.com>
2024-12-18 21:34:31 +0700

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -30,20 +30,28 @@ class House:
             return self.number_of_floors < other
 
     def __le__(self, other):
-        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+        if isinstance(other, House):
             return self.number_of_floors <= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors <= other
 
     def __gt__(self, other):
-        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+        if isinstance(other, House):
             return self.number_of_floors > other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors > other
 
     def __ge__(self, other):
-        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+        if isinstance(other, House):
             return self.number_of_floors >= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors >= other
 
     def __ne__(self, other):
-        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+        if isinstance(other, House):
             return self.number_of_floors != other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors != other
 
     def __add__(self, value):
         if isinstance(value, int):
@@ -72,7 +80,7 @@ print(h1)
 print(h1 == h2)
 
 h1 += 10 #__iadd__
-print(h1) 
+print(h1)
 
 h2 = 10 + h2 #__radd__
 print(h2)
